09b0517bfeef4f2785daf77be6cc1a46f47a26fd
Daian Dragos <daiandragos@Daians-MacBook-Air.local>
2023-10-07 11:13:39 +0300

    initial commit

---
+++ app/api/users/schema.ts
@@ -0,0 +1,5 @@
+// validate using zod package
+
+import { z } from "zod";
+const schema = z.object({ name: z.string().min(3), email: z.string().email() });
+export default schema;
---
+++ app/components/AddToCart.tsx
@@ -0,0 +1,12 @@
+'use client'
+import React from "react";
+
+const AddToCart = () => {
+  return (
+    <div>
+      <button className="btn btn-primary" onClick={() => console.log("aaa")}>Add to card</button>
+    </div>
+  );
+};
+
+export default AddToCart;
